546086a8e638698686d49a7e58ce42c5f9fcecf0
Bhargav <bhargav.kulkarni@walchandsangli.ac.in>
2021-08-03 17:43:22 +0530

    initial commit

---
+++ app/src/main/java/com/example/the_project/CreateQuizTeacherActivity.java
@@ -0,0 +1,27 @@
+package com.example.the_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.the_project.databinding.ActivityCreateQuizTeacherBinding;
+
+public class CreateQuizTeacherActivity extends AppCompatActivity {
+
+    ActivityCreateQuizTeacherBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_quiz_teacher);
+        binding =ActivityCreateQuizTeacherBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.IdAddQuestionCreateQuizTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
